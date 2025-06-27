86d36f6a9644cf511e93d74b8367ebac6367bf56
tulliesummers <chaffinedu@gmail.com>
2025-06-27 14:52:14 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-VANTA.WAVES({
+VANTA.FOG({
   el: "#vanta-bg",
   mouseControls: true,
   touchControls: true,
   minHeight: 200.00,
   minWidth: 200.00,
-  color: 0x333366,
-  shininess: 50.00,
-  waveHeight: 20.00,
-  waveSpeed: 0.5,
-  zoom: 1.00
+  highlightColor: 0xaa0000,
+  midtoneColor: 0x330000,
+  lowlightColor: 0x000000,
+  baseColor: 0x000000,
+  blurFactor: 0.6,
+  speed: 1.2,
+  zoom: 0.7
 });
