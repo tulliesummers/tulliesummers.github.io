7d5171eef22afed86e411cf9f2f749a2d3c8b6e0
tulliesummers <chaffinedu@gmail.com>
2025-06-27 12:00:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+VANTA.WAVES({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  color: 0x333366,
+  shininess: 50.00,
+  waveHeight: 20.00,
+  waveSpeed: 0.5,
+  zoom: 1.00
+});
